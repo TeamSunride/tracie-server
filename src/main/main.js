35c1a764b41a91ee1b068a8077f35ef1d2092fdc
Starman <30315137+Starman3787@users.noreply.github.com>
2024-12-19 15:52:08 +0000

    remove commented out code

--- src/main/main.js
+++ src/main/main.js
@@ -84,41 +84,9 @@ const createWindow = async () => {
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
-      // nodeIntegration: true,
-      // contextIsolation: false,
     },
     titleBarStyle: 'hidden',
   });
-
-  // mainWindow.webContents.session.on(
-  //   'select-serial-port',
-  //   (event, portList, webContents, callback) => {
-  //     // Add listeners to handle ports being added or removed before the callback for `select-serial-port`
-  //     // is called.
-  //     mainWindow.webContents.session.on(
-  //       'serial-port-added',
-  //       (addEvent, port) => {
-  //         console.log('serial-port-added FIRED WITH', port);
-  //         // Optionally update portList to add the new port
-  //       },
-  //     );
-
-  //     mainWindow.webContents.session.on(
-  //       'serial-port-removed',
-  //       (removeEvent, port) => {
-  //         console.log('serial-port-removed FIRED WITH', port);
-  //         // Optionally update portList to remove the port
-  //       },
-  //     );
-
-  //     event.preventDefault();
-  //     if (portList && portList.length > 0) {
-  //       callback(portList[0].portId);
-  //     } else {
-  //       callback(''); // Could not find any matching devices
-  //     }
-  //   },
-  // );
 
   mainWindow.webContents.session.setPermissionCheckHandler(
     (webContents, permission, requestingOrigin, details) => {
